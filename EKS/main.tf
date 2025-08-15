433f74ce21e1c6438e2e1d6fd60a183fc8119c7f
yogesh kawtikwar <yogesh.kawtikwar@gmail.com>
2025-08-15 12:31:59 +0530

    upadted the file

--- EKS/main.tf
+++ EKS/main.tf
@@ -29,56 +29,6 @@ module "vpc" {
 
 }
 
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
-
-#   cluster_name    = "my-eks-cluster"
-#   cluster_version = "1.24"
-
-#   cluster_endpoint_public_access = true
-
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-
-#   eks_managed_node_groups = {
-#     nodes = {
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 2
-
-#       instance_type = ["t2.micro"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "dev"
-#     Terraform   = "true"
-#   }
-# }
-
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "19.15.3"  # or latest available
-
-#   cluster = {
-#     name    = "my-eks-cluster"
-#     version = "1.24"
-#     endpoint_public_access = true
-#   }
-
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-
-#   eks_managed_node_groups = {
-#     nodes = {
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 2
-#       instance_types = ["t2.micro"] # plural in v19+
-#     }
-#   }
-# }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
@@ -102,7 +52,7 @@ module "eks" {
     eks_managed_node_groups = {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
