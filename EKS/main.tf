8e027efd3ee531c0f685dfdb70f2dff77e5b088b
yogesh kawtikwar <yogesh.kawtikwar@gmail.com>
2025-08-15 14:56:31 +0530

    upadted the file

--- EKS/main.tf
+++ EKS/main.tf
@@ -29,30 +29,68 @@ module "vpc" {
 
 }
 
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
+
+#   # Cluster settings
+#   name               = "my-eks-cluster"
+#   kubernetes_version = "1.33"
+#   endpoint_public_access = true
+
+#   # Optional: grants current IAM caller admin access
+#   enable_cluster_creator_admin_permissions = true
+
+#   # Compute (node groups)
+#   compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose"]
+#   }
+
+#   # Node pool definitions
+
+#     eks_managed_node_groups = {
+#     example = {
+#       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+#       instance_types = ["t3.small"]
+
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 2
+#     }
+#   }
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  # Cluster settings
-  name               = "my-eks-cluster"
+  name               = "my-cluster"
   kubernetes_version = "1.33"
+
+  # Optional
   endpoint_public_access = true
 
-  # Optional: grants current IAM caller admin access
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  # Compute (node groups)
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  # Node pool definitions
-
-    eks_managed_node_groups = {
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      instance_types = ["t3.small"]
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.small"]
 
       min_size     = 1
       max_size     = 3
@@ -60,13 +98,8 @@ module "eks" {
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
-
-
